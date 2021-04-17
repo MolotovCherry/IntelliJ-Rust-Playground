9dec686f37622dc16d6df5bf97ef265176962148
Cherryleafroad <13651622+cherryleafroad@users.noreply.github.com>
2021-04-16 22:14:16 -0700

    Now it's context menu aware

--- src/main/kotlin/com/cherryleafroad/rust/playground/actions/RunAction.kt
+++ src/main/kotlin/com/cherryleafroad/rust/playground/actions/RunAction.kt
@@ -3,40 +3,47 @@ package com.cherryleafroad.rust.playground.actions
 import com.cherryleafroad.rust.playground.Helpers
 import com.cherryleafroad.rust.playground.PatchCargoCommandLine
 import com.intellij.ide.scratch.ScratchUtil
+import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
 import org.rust.cargo.project.model.cargoProjects
 import org.rust.lang.core.psi.isRustFile
-import org.rust.openapiext.virtualFile
+import org.rust.openapiext.document
+import org.rust.openapiext.psiFile
 
 class RunAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
         val project = event.project!!
 
+        val doc = event.dataContext.psiFile?.virtualFile ?: return
+
+        // this could execute with a shortcut for example
+        val isRust = doc.isRustFile
+        val isScratch = ScratchUtil.isScratch(doc)
+        // this COULD trigger if you do a shortcut for example
+        if (!isRust || !isScratch) {
+            return
+        }
+
         val cargoPlayInstalled = Helpers.checkCargoPlayInstalled(project)
         if (cargoPlayInstalled) {
-            val code = event.getData(LangDataKeys.EDITOR)?.document?.text
-            if (code != null) {
-                val cargoProject = project.cargoProjects.allProjects.firstOrNull() ?: return
+            val code = doc.document!!.text
+            val cargoProject = project.cargoProjects.allProjects.firstOrNull() ?: return
 
-                val doc = FileEditorManager.getInstance(project).selectedTextEditor?.document!!.virtualFile!!
-                val cwd = doc.toNioPath().parent
-                val fileName = doc.name
+            val cwd = doc.toNioPath().parent
+            val fileName = doc.name
 
-                val (args, toolchain) = Helpers.parseScratch(fileName, code)
+            val (args, toolchain) = Helpers.parseScratch(fileName, code)
 
-                val commandLine = PatchCargoCommandLine(
-                    "play",
-                    cwd,
-                    args,
-                    channel = toolchain
-                )
-                commandLine.run(cargoProject, "Play $fileName", saveConfiguration = false)
-            }
+            val commandLine = PatchCargoCommandLine(
+                "play",
+                cwd,
+                args,
+                channel = toolchain
+            )
+            commandLine.run(cargoProject, "Play $fileName", saveConfiguration = false)
         } else {
             Helpers.cargoPlayInstallNotification(project)
         }
@@ -45,16 +52,21 @@ class RunAction : AnAction() {
     override fun update(e: AnActionEvent) {
         // Set the availability based on whether a project is open
         val project: Project? = e.project
-        e.presentation.isVisible = true
+        e.presentation.isVisible = e.place == ActionPlaces.MAIN_MENU
+        e.presentation.isEnabled = false
 
         if (project != null) {
-            val currentDoc = FileEditorManager.getInstance(project).selectedTextEditor?.document
-            if (currentDoc != null) {
-                val currentFile = FileDocumentManager.getInstance().getFile(currentDoc)
-                val isRust = currentFile?.isRustFile ?: false
-                val isScratch = ScratchUtil.isScratch(currentFile)
+            e.dataContext.psiFile?.virtualFile?.let {
+                if (e.place == ActionPlaces.EDITOR_POPUP || e.place == ActionPlaces.MAIN_MENU || e.place == ActionPlaces.PROJECT_VIEW_POPUP) {
+                    val isRust = it.isRustFile
+                    val isScratch = ScratchUtil.isScratch(it)
 
-                e.presentation.isEnabled = isRust && isScratch
+                    e.presentation.isEnabledAndVisible = isRust && isScratch
+
+                    if (e.place == ActionPlaces.MAIN_MENU && !(isRust && isScratch)) {
+                        e.presentation.isVisible = true
+                    }
+                }
             }
         }
     }
