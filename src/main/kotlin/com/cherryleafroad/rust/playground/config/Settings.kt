9d0d89d1116617d24caec4506400b1ccdff8f0b6
Cherryleafroad <13651622+cherryleafroad@users.noreply.github.com>
2021-04-21 22:37:22 -0700

    Change default scratch message as it's no longer required

--- src/main/kotlin/com/cherryleafroad/rust/playground/config/Settings.kt
+++ src/main/kotlin/com/cherryleafroad/rust/playground/config/Settings.kt
@@ -19,36 +19,9 @@ object Settings {
 
     val DEFAULT_TEXT =
         """
-            /* Specify cargo-play features to use with //@ . Use space separators for single args.
-             *   _Flags: check(c) clean expand infer(i) quiet(q) release test verbose(v)_
-             *
-             *     //@ expand release quiet i
-             * 
-             * Options can be specified on the same //@ line, but need comma separators between opts.
-             * src adds additional files to compile with the main file (CWD is scratch directory, so
-             * you can include other scratch files easily)
-             *   _Options: cargo-option edition(e) mode(m) src_
-             *
-             *     //@ cargo-option --verbose --quiet, src scratch2.rs scratch3.rs, mode build
-             *
-             * You can also override the default playground toolchain by using
-             *     //@ toolchain <toolchain>
-             * Where <Toolchain> is one of (case-insensitive): DEFAULT, STABLE, BETA, NIGHTLY, DEV
-             *
-             * Full example (note: opts are always at the end)
-             *     //@ expand release q, edition 2018, m run, src scratch2.rs scratch3.rs
-             *
-             * //@ must be the first line, and //$ can be first or second line. Long flags are
-             * case-insensitive, where short flags are case sensitive
-             *
-             * Specify program args //$
-             *     //$ --my-arg 5
-             *
-             * Specify external dependencies with //# . It uses the same TOML syntax as Cargo.toml
-             *     //# serde_json = "*"
-             *
-             * For more information, check out cargo-play docs
-             * Change the default Rust scratch message in Rust settings page
+            /*
+             * For more information, check out the cargo-play docs
+             * You can change the default Rust scratch message in Rust settings page
              */
             
             fn main() {
